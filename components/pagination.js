fbc0739027adc3c091df644aa9f610f91c939e79
Ylerson Almonte <yacwarefree1@gmail.com>
2023-03-12 22:23:34 -0400

    Create Components Pagination

---
+++ components/pagination.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Pagination = ({
+    currentPage,
+    itemsPerPage,
+    totalItems,
+    onPageChange,
+    displayPageCount,
+}) => {
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const pageNumbers = Array.from({length: totalPages}, (_, i) => i + 1);
+
+    let visiblePages = pageNumbers.slice(0, displayPageCount);
+
+    if (currentPage > Math.floor(displayPageCount / 2)) {
+        const startIndex = currentPage - Math.floor(displayPageCount / 2) - 1;
+        visiblePages = pageNumbers.slice(
+            startIndex,
+            startIndex + displayPageCount
+        );
+    }
+
+    return (
+        <div className="flex justify-center items-center gap-2">
+            <button
+                className="bg-white shadow rounded-md px-4 "
+                disabled={currentPage === 1}
+                onClick={() => onPageChange(currentPage - 1)}
+            >
+                ⟪
+            </button>
+            <button
+                onClick={() => onPageChange(1)}
+                className="bg-white shadow rounded-md px-4 "
+            >
+                ⟪⟪
+            </button>
+
+            {visiblePages.map((pageNumber) => (
+                <button
+                    key={pageNumber}
+                    className={`bg-white shadow rounded-md px-4 py-2 ${
+                        currentPage === pageNumber
+                            ? 'bg-blue-500 text-white'
+                            : 'hover:bg-gray-200'
+                    }`}
+                    onClick={() => onPageChange(pageNumber)}
+                >
+                    {pageNumber}
+                </button>
+            ))}
+
+            <button
+                className={
+                    currentPage === totalPages
+                        ? 'hidden'
+                        : 'bg-white shadow rounded-md px-4 text-center'
+                }
+                disabled={currentPage === totalPages}
+                onClick={() => onPageChange(currentPage + 1)}
+            >
+                ⟫
+            </button>
+        </div>
+    );
+};
+
+export default Pagination;
